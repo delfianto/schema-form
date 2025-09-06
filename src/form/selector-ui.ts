e6ee3e3cfbbc3206f766849fd454ff0bb8f72941
Dwi Elfianto <2359896+delfianto@users.noreply.github.com>
2025-09-07 02:29:21 +0700

    Always resolve promise in FileSelectorModal

--- src/form/selector-ui.ts
+++ src/form/selector-ui.ts
@@ -1,45 +1,62 @@
-import { App, Modal, TFile, Setting } from "obsidian";
+import { App, Modal, Setting, TFile } from "obsidian";
 
 export class FileSelectorModal extends Modal {
-	files: TFile[];
-	onChoose: (file: TFile | null) => void;
+  files: TFile[];
+  onChoose: (file: TFile | null) => void;
+  private resolved = false;
 
-	constructor(
-		app: App,
-		files: TFile[],
-		onChoose: (file: TFile | null) => void,
-	) {
-		super(app);
-		this.files = files;
-		this.onChoose = onChoose;
-	}
+  constructor(
+    app: App,
+    files: TFile[],
+    onChoose: (file: TFile | null) => void,
+  ) {
+    super(app);
+    this.files = files;
+    this.onChoose = onChoose;
+  }
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.createEl("h3", { text: "Choose a schema file:" });
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.createEl("h3", { text: "Choose a schema file:" });
 
-		this.files.forEach((file) => {
-			new Setting(contentEl)
-				.setName(file.name)
-				.setDesc(file.path)
-				.addButton((btn) =>
-					btn.setButtonText("Select").onClick(() => {
-						this.onChoose(file);
-						this.close();
-					}),
-				);
-		});
+    this.files.forEach((file) => {
+      new Setting(contentEl)
+        .setName(file.name)
+        .setDesc(file.path)
+        .addButton((btn) =>
+          btn.setButtonText("Select").onClick(() => {
+            this.resolve(file)
+          }),
+        );
+    });
 
-		// Cancel button
-		new Setting(contentEl).addButton((btn) =>
-			btn.setButtonText("Cancel").onClick(() => {
-				this.onChoose(null);
-				this.close();
-			}),
-		);
-	}
+    // Cancel button
+    new Setting(contentEl).addButton((btn) =>
+      btn.setButtonText("Cancel").onClick(() => {
+        this.resolve();
+      }),
+    );
+  }
 
-	onClose() {
-		this.contentEl.empty();
-	}
+  onClose() {
+    this.contentEl.empty();
+
+    if (!this.resolved) {
+      this.resolved = true;
+      this.onChoose(null);
+    }
+  }
+
+  resolve(file?: TFile) {
+    if (!this.resolved) {
+      if (file != undefined) {
+        this.onChoose(file);
+      } else {
+        this.onChoose(null);
+      }
+
+      this.resolved = true;
+      this.close();
+    }
+  }
 }
